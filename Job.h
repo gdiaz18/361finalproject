09d69cdf57b5be45dcdd87e76f459ff95e47464c
gdiaz18 <giadiaz@udel.edu>
2023-05-15 20:39:28 -0400

    fixed struct issue

--- Job.h
+++ Job.h
@@ -1,7 +1,15 @@
 #define SJF_Queue 1
 #define FIFO_Queue 2
 
-typedef struct Job{
+#include "command.h"
+
+#ifndef JOB_H
+#define JOB_H
+
+//typedef struct Job Job;
+//typedef struct Queue Queue;
+
+struct Job{
     int jobId;
     int priority;
     int needMemory;
@@ -12,22 +20,24 @@ typedef struct Job{
     //int leftTime;
     int arrivalTime;
     int leaveTime;
-    Job* next;
-}Job;
+    struct Job* next;
+};
 
-typedef struct Queue{
-    Job* head;
-    Job* tail;
+struct Queue{
+    struct Job* head;
+    struct Job* tail;
     int queueType; // 1: SJF 2:FIFO
     int size;
-}Queue;
+};
 
 
 //main.c functions
-Job* newJob(Command* c);
-void displayJob(Job* j, int type);
-Queue* newQueue(int type);
-void pushQueue(Queue* q, Job* j);
-Job* popQueue(Queue* q);
-int emptyQueue(Queue*q);
-void displayJob(Job* j, int type);
+struct Job* newJob(struct Command* c);
+void displayJob(struct Job* j, int type);
+struct Queue* newQueue(int type);
+void pushQueue(struct Queue* q, struct Job* j);
+struct Job* popQueue(struct Queue* q);
+int emptyQueue(struct Queue* q);
+void displayJob(struct Job* j, int type);
+
+#endif /*JOB_H*/
